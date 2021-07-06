72f5a9f1fd4888219315e4a97c04f8b4e4a37a9c
Shyam Madhusudhana <skm246@cornell.edu>
2021-07-06 12:51:00 -0400

    renamed inference file

--- koogu/__init__.py
+++ koogu/__init__.py
@@ -1,5 +1,5 @@
 from . import prepare_data as prepare
 from .train_and_eval import train_and_eval as train
-from .predict import recognize
+from .inference import recognize
 
-__version__ = '0.3.9'
+__version__ = '0.4.0'
