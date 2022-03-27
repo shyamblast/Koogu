790232dc32953a1a3518769d43821758b293eda7
Shyam Madhusudhana <skm246@cornell.edu>
2022-03-27 11:25:02 -0400

    early gh-pages attempts

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -24,7 +24,7 @@ set -x
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-stemmer python3-git python3-pip python3-virtualenv python3-setuptools
 
-python3 -m pip install --upgrade rinohtype GitPython
+python3 -m pip install --upgrade -r docs/requirements.txt
 
 #####################
 # DECLARE VARIABLES #
