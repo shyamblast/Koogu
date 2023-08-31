93104a2f83ea421c6f72f1bb29d2ec0c6f5c7558
Shyam Madhusudhana <skm246@cornell.edu>
2023-09-01 00:20:12 +0400

    Added tests for LoG transformation

--- tests/data/test_tf_transformations.py
+++ tests/data/test_tf_transformations.py
@@ -5,7 +5,7 @@ from matplotlib import colormaps
 import pytest
 from koogu.data.raw import Settings, Convert
 from koogu.data.tf_transformations import \
-    Audio2Spectral, Spec2Img, NormalizeAudio, GaussianBlur
+    Audio2Spectral, Spec2Img, NormalizeAudio, GaussianBlur, LoG
 from tests.data import save_display_to_disk
 
 
@@ -136,3 +136,83 @@ def test_GaussianBlur(narw_json_clips_and_settings, outputroot,
         outputs, outputroot, 'test_tf_transformations',
         'GaussianBlur_' + ('xy' if apply_2d else 'y') +
         ('_tf' if tf_spec else ''))
+
+
+@pytest.mark.parametrize('add_offsets, with_conv, retain_LoG, tf_spec', [
+    (True, False, False, False),    # 1. Output should be same as 2
+    (False, False, False, False),   # 2
+    (True, False, False, True),     # 3. Output should be same as 4 & 1
+    (False, False, False, True),    # 4
+    (False, False, True, True),     # 5. Output should be same as 4
+    (False, True, False, False),    # 6. Only conv outputs
+    (False, True, False, True),     # 7. Only conv outputs
+    (False, True, True, False),     # 8. Log + conv outputs
+    (False, True, True, True),      # 9. Log + conv outputs
+])
+def test_LoG(narw_json_clips_and_settings, outputroot,
+             add_offsets, with_conv, retain_LoG, tf_spec):
+    """
+
+    Args:
+        narw_json_clips_and_settings:
+        outputroot:
+        add_offsets:
+        tf_spec: If True, test TF Audio2Spectral, else test scipy fft
+    """
+
+    data, fs, spec_settings, expected_spec_shape = narw_json_clips_and_settings
+    scales_sigmas = (4, 8, 16)
+    conv_filters = 3
+
+    if with_conv:
+        tf.random.set_seed(37)
+        num_output_chs = (conv_filters * len(scales_sigmas)) + (
+            len(scales_sigmas) if retain_LoG else 0)
+    else:
+        num_output_chs = len(scales_sigmas)
+
+    orig_clips = Convert.pcm2float(np.stack([data[0, ...], data[1, ...]]))
+
+    outputs = dict()
+    if tf_spec:
+        orig_specs = Audio2Spectral(fs, spec_settings)(orig_clips)
+        outputs['Original'] = orig_specs.numpy()
+        orig_shape = orig_specs.shape
+        orig_specs = tf.expand_dims(orig_specs, -1)
+    else:
+        spec_settings_c = Settings.Spectral(fs, **spec_settings)
+        orig_specs = Convert.audio2spectral(orig_clips, fs, spec_settings_c)
+        outputs['Original'] = orig_specs
+        orig_shape = orig_specs.shape
+        orig_specs = np.expand_dims(orig_specs, -1)
+
+    l_outputs = LoG(scales_sigmas,
+                    add_offsets=add_offsets,
+                    conv_filters=conv_filters if with_conv else None,
+                    retain_LoG=retain_LoG
+                    )(orig_specs).numpy()
+
+    assert num_output_chs == l_outputs.shape[-1], 'Num channels mismatch'
+    assert np.all(orig_shape == l_outputs.shape[:-1]), \
+        f'{orig_shape} != {l_outputs.shape[:-1]}'
+
+    s_idx_offset = 0
+    if (with_conv and retain_LoG) or (not with_conv):
+        for s_idx, s in enumerate(scales_sigmas):
+            outputs[f's={s}'] = l_outputs[:, :, :, s_idx]
+        s_idx_offset = len(scales_sigmas)
+    if with_conv:
+        for s_idx, s in enumerate(scales_sigmas):
+            for c_idx in range(conv_filters):
+                outputs[f's={s}, c={c_idx+1}'] = \
+                    l_outputs[:, :, :, s_idx_offset + s_idx + c_idx]
+
+    save_display_to_disk(
+        outputs, outputroot, 'test_tf_transformations',
+        'LoG' + (
+            '_offsets' if add_offsets else '') + (
+            '_c' if with_conv else '') + (
+            '_r' if retain_LoG else '') + (
+            '_tf' if tf_spec else ''),
+        normalize_levels=False
+    )
