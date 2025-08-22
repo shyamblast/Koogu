6275ba41afafcc2326e65fc55ad5f8fcd62214ec
Shyam Madhusudhana <skm246@cornell.edu>
2025-08-23 00:21:31 +0400

    proper setting of version string for a pre-pre-release testing version

--- koogu/__init__.py
+++ koogu/__init__.py
@@ -2,6 +2,6 @@ from .train_and_eval import train_and_eval as train
 from .inference import recognize
 from .utils import assessments
 
-__version__ = '0.8.0rc'
+__version__ = '0.8.0-alpha'
 
 __all__ = ['prepare', 'train', 'recognize', 'assessments']
